da3acabf387c951bbab6f6b5a1504bce4ca11bc2
Yang907 <67835698+Yang907@users.noreply.github.com>
2025-10-17 16:30:06 +0800

    Add files via upload

---
+++ lineData.js
@@ -0,0 +1,168 @@
+const lineData = [
+  {
+    lineCode: "4",
+    lineColor: "#00af60",
+    siteList: [
+      {
+        sitecode: "4-00",
+        siteName: "延长线",
+        position: { x: 50, y: 100 },
+      },
+      {
+        sitecode: "4-01",
+        siteName: "万盛",
+        position: { x: 100, y: 100 },
+      },
+      {
+        sitecode: "4-02",
+        siteName: "杨柳河",
+        position: { x: 150, y: 100 },
+      },
+      {
+        sitecode: "4-03",
+        siteName: "凤溪河",
+        position: { x: 200, y: 100 },
+      },
+      {
+        sitecode: "4-04",
+        siteName: "南薰大道",
+        position: { x: 250, y: 100 },
+      },
+      {
+        sitecode: "4-05",
+        siteName: "光华公园",
+        position: { x: 300, y: 100 },
+      },
+      {
+        sitecode: "4-06",
+        siteName: "涌泉",
+        position: { x: 350, y: 100 },
+      },
+      {
+        sitecode: "4-07",
+        siteName: "凤凰大街",
+        position: { x: 400, y: 100 },
+      },
+      {
+        sitecode: "4-08",
+        siteName: "马厂坝",
+        position: { x: 450, y: 100 },
+      },
+      {
+        sitecode: "4-09",
+        siteName: "非遗博览园",
+        position: { x: 500, y: 100 },
+      },
+      {
+        sitecode: "4-10",
+        siteName: "蔡桥",
+        position: { x: 550, y: 100 },
+      },
+      {
+        sitecode: "4-11",
+        siteName: "中坝",
+        position: { x: 600, y: 100 },
+      },
+      {
+        sitecode: "4-12",
+        siteName: "成都西站",
+        position: { x: 650, y: 100 },
+      },
+      {
+        sitecode: "4-13",
+        siteName: "清江西路",
+        position: { x: 700, y: 100 },
+      },
+      {
+        sitecode: "4-14",
+        siteName: "文化宫",
+        position: { x: 750, y: 100 },
+      },
+      {
+        sitecode: "4-15",
+        siteName: "西南财大",
+        position: { x: 800, y: 100 },
+      },
+      {
+        sitecode: "4-16",
+        siteName: "草堂北路",
+        position: { x: 850, y: 100 },
+      },
+      {
+        sitecode: "4-17",
+        siteName: "中医大省医院",
+        position: { x: 900, y: 100 },
+      },
+      {
+        sitecode: "4-18",
+        siteName: "宽窄巷子",
+        position: { x: 950, y: 100 },
+      },
+      {
+        sitecode: "4-19",
+        siteName: "骡马市",
+        position: { x: 1000, y: 100 },
+      },
+      {
+        sitecode: "4-20",
+        siteName: "太升南路",
+        position: { x: 1050, y: 110 },
+      },
+      {
+        sitecode: "4-22",
+        siteName: "市二医院",
+        position: { x: 1100, y: 150 },
+      },
+      {
+        sitecode: "4-21",
+        siteName: "玉双路",
+        position: { x: 1150, y: 200 },
+      },
+      {
+        sitecode: "4-22",
+        siteName: "双桥路",
+        position: { x: 1200, y: 240 },
+      },
+      {
+        sitecode: "4-23",
+        siteName: "万年场",
+        position: { x: 1250, y: 250 },
+      },
+      {
+        sitecode: "4-24",
+        siteName: "槐树店",
+        position: { x: 1300, y: 250 },
+      },
+      {
+        sitecode: "4-25",
+        siteName: "来龙",
+        position: { x: 1350, y: 250 },
+      },
+      {
+        sitecode: "4-26",
+        siteName: "十陵",
+        position: { x: 1400, y: 250 },
+      },
+      {
+        sitecode: "4-27",
+        siteName: "成都大学",
+        position: { x: 1450, y: 250 },
+      },
+      {
+        sitecode: "4-28",
+        siteName: "明蜀王陵",
+        position: { x: 1500, y: 250 },
+      },
+      {
+        sitecode: "4-29",
+        siteName: "西河",
+        position: { x: 1550, y: 250 },
+      },
+      {
+        sitecode: "4-30",
+        siteName: "延长线",
+        position: { x: 1600, y: 250 },
+      },
+    ],
+  },
+];
